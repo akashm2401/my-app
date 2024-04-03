b06e4c8e905b7d300db692e29d2abf1e5b256d7c
Akash More <akashkmore2002@gmail.com>
2024-04-04 00:27:43 +0530

    date issue fixed 2

--- src/js/countdown1.js
+++ src/js/countdown1.js
@@ -1,3 +1,11 @@
+let date = new Date();
+console.log(date);
+if (date <=  new Date("2024-03-04")){
+    window.location.href="src/template/countdown1.html"
+} else {
+    window.location.href="src/template/wish.html"
+}
+
 
 // To manipulate count down time 
 function setCountdown(oldDays, oldHours, oldminutes) {
