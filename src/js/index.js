6ed3d5ddf69534a017ad540c8f6913e3c253f383
Akash More <akashkmore2002@gmail.com>
2024-04-04 00:24:55 +0530

    dae issue solved

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 let date = new Date();
 console.log(date);
-if (date < new Date("2024-04-04")){
+if (date <=  new Date("2024-03-04")){
     window.location.href="src/template/countdown1.html"
 } else {
     window.location.href="src/template/wish.html"
