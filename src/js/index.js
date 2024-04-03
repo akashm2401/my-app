daa28840abdb5243b214dc4876b622275cf716e9
Akash More <akashkmore2002@gmail.com>
2024-04-03 23:34:31 +0530

    2nd last commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,6 @@ let date = new Date();
 console.log(date);
 if (date < new Date("2024-04-04")){
     window.location.href="src/template/countdown1.html"
+} else {
+    window.location.href="src/template/wish.html"
 }
